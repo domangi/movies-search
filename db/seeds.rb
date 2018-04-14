bf5df92f889fe985dcddc7ddda6bfbcf29d7ef61
Domenico Angilletta <domenico.angilletta@gmail.com>
2018-04-14 12:04:29 +0200

    Add Movie Data import to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+movie_attrs = []
+CSV.foreach(Rails.root.join("movies_metadata.csv"), { :headers => true }) do |csv|
+  title, overview, image_path, vote_average = csv[20], csv[9],  csv[11], csv[22]
+
+  image_url = image_path.nil? ? "" : "https://image.tmdb.org/t/p/w300" + image_path
+
+  movie_attrs << {
+    title: title,
+    overview: overview,
+    image_url: image_url,
+    vote_average: vote_average
+  }
+end
+
+Movie.bulk_insert do |worker|
+  movie_attrs.each do |attrs|
+    worker.add(attrs)
+  end
+end
